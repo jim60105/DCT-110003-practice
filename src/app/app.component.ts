cf9212ea5eb66d6831d39e7bbac8c727ab19b998
陳鈞 <jim60105@gmail.com>
2021-03-14 14:43:24 +0800

    任務11：自訂服務元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ArticleService } from './article.service';
+import { Component, OnInit } from '@angular/core';
 import { Article } from './Article';
 
 @Component({
@@ -6,71 +7,19 @@ import { Article } from './Article';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'New Title';
   subTitle = 'A place to share your <u>knowledge</u>.';
 
-  rawList: Article[] = [
-    {
-      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-      slug: 'zp7yqc',
-      body: 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium',
-      createdAt: '2018-05-11T21:58:27.358Z',
-      updatedAt: '2018-05-11T21:58:27.358Z',
-      tagList: [],
-      description: 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium',
-      author: {
-        username: 'Eliseo@gardner.biz',
-        bio: 'Eliseo',
-        image: 'http://placekitten.com/200/300',
-        following: false
-      },
-      favorited: false,
-      favoritesCount: 1
-    },
-    {
-      title: 'quo vero reiciendis velit similique earum',
-      slug: 'p3vcsw',
-      body: 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
-      createdAt: '2018-05-11T21:55:00.722Z',
-      updatedAt: '2018-05-11T21:55:00.722Z',
-      tagList: [],
-      description: 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
-      author: {
-        username: 'Jayne_Kuhic@sydney.com',
-        bio: 'Jayne_Kuhic',
-        image: 'http://placekitten.com/200/300',
-        following: false
-      },
-      favorited: false,
-      favoritesCount: 3
-    },
-    {
-      title: 'odio adipisci rerum aut animi',
-      slug: '3c69lg',
-      body: 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
-      createdAt: '2018-05-11T19:49:35.917Z',
-      updatedAt: '2018-05-11T19:49:35.917Z',
-      tagList: [],
-      description: 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
-      author: {
-        username: 'Lew@alysha.tv',
-        bio: 'Lew',
-        image: 'http://placekitten.com/200/300',
-        following: false
-      },
-      favorited: false,
-      favoritesCount: 5
-    }
-  ];
+  list: Article[];
 
-  list: Article[] = [].concat(this.rawList);
+  constructor(private articleService: ArticleService) {
+  }
+  ngOnInit(): void {
+    this.list = this.articleService.getRawList();
+  }
+
   doSearch(search: string): void {
-    this.list.length = 0;
-    this.rawList.forEach((element: Article) => {
-      if (element.title.indexOf(search) >= 0) {
-        this.list.push(element);
-      }
-    });
+    this.list = this.articleService.doSearch(search);
   }
 }
